2bb6778538ee408ee4114877636edfaf0df3ae3c
Aaditya Sharma <aadityasharma08072004@gmail.com>
2024-11-01 19:21:36 +0530

    Update package.json and vite.config.js for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // This will expose the server to the network
+    port: 5173, 
+  },
 })
